5c177a6f69cc247b19180a36994e4e20baadf9c0
LouiseOdier <maarit.vilen@helsinki.fi>
2025-11-13 07:35:30 +0200

    project installation, hello world

--- src/app.py
+++ src/app.py
@@ -1,13 +1,17 @@
-from flask import redirect, render_template, request, jsonify, flash
-from db_helper import reset_db
-from repositories.todo_repository import get_todos, create_todo, set_done
-from config import app, test_env
-from util import validate_todo
+from flask import Flask, redirect, render_template, request, jsonify, flash
+#from db_helper import reset_db
+#from repositories.todo_repository import get_todos, create_todo, set_done
+#from config import app, test_env
+#from util import validate_todo
+
+app = Flask(__name__)
 
 @app.route("/")
+
 def index():
-    todos = get_todos()
-    unfinished = len([todo for todo in todos if not todo.done])
-    return render_template("index.html", todos=todos, unfinished=unfinished) 
+    #todos = get_todos()
+    #unfinished = len([todo for todo in todos if not todo.done])
+    #return render_template("index.html", todos=todos, unfinished=unfinished)
+    return render_template("index.html") 
 
 
