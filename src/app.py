b63a716c89819c9ddc0e5f75f3428deeac205019
Kooville <ville.kuhlberg@helsinki.fi>
2025-11-16 20:48:45 +0200

    template viitteen lisäykselle ja linkki siihen etusivulta

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, render_template, request, jsonify, flash
+#from config import db
 #from db_helper import reset_db
 #from repositories.todo_repository import get_todos, create_todo, set_done
 #from config import app, test_env
@@ -13,5 +14,38 @@ def index():
     #unfinished = len([todo for todo in todos if not todo.done])
     #return render_template("index.html", todos=todos, unfinished=unfinished)
     return render_template("index.html") 
+
+
+@app.route('/books/new')
+def new_book():
+    return render_template('book_form.html')
+
+
+@app.route('/books/create', methods=['POST'])
+def create_book():
+    title = request.form.get('title')
+    author = request.form.get('author')
+    year = request.form.get('year')
+    isbn = request.form.get('isbn')
+    publisher = request.form.get('publisher')
+
+    # Minimal validation: title and author required
+    if not title or not author:
+        flash('Title and author are required.', 'error')
+        return redirect('/books/new')
+
+    sql = "INSERT INTO items (title, writer, year, isbn, publisher) VALUES (:title, :writer, :year, :isbn, :publisher)"
+    
+    #db.session.execute(sql, {
+    #    'title': title,
+    #    'writer': author,
+    #    'year': year,
+    #    'isbn': isbn,
+    #    'publisher': publisher
+    #})
+    #db.session.commit()
+
+    flash('Kirja lisätty tietokantaan.', 'success')
+    return redirect('/')
 
 
