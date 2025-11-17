ff877999f1bea33164297a9c532dbb1e8069970d
OttoSFelix <otto.suokas@helsinki.fi>
2025-11-17 14:16:21 +0200

    etusivulla näkyy lisätyt kirjat

--- src/app.py
+++ src/app.py
@@ -1,19 +1,27 @@
 from flask import Flask, redirect, render_template, request, jsonify, flash
-#from config import db
-#from db_helper import reset_db
-#from repositories.todo_repository import get_todos, create_todo, set_done
-#from config import app, test_env
-#from util import validate_todo
+from config import db
+from db_helper import reset_db
+from repositories.todo_repository import get_todos, create_todo, set_done
+from config import app, test_env
+from util import validate_todo
+import os
+from dotenv import load_dotenv
+from config import text
 
-app = Flask(__name__)
+
+load_dotenv()
+app.secret_key = os.getenv("SECRET_KEY")
+
+if not app.secret_key:
+    print("WARNING: Secret key not found. Check your .env file.")
 
 @app.route("/")
 
 def index():
-    #todos = get_todos()
-    #unfinished = len([todo for todo in todos if not todo.done])
-    #return render_template("index.html", todos=todos, unfinished=unfinished)
-    return render_template("index.html") 
+    sql = text("SELECT * FROM items")
+    query = db.session.execute(sql)
+    items = query.fetchall()
+    return render_template("index.html", items=items) 
 
 
 @app.route('/books/new')
@@ -34,16 +42,15 @@ def create_book():
         flash('Title and author are required.', 'error')
         return redirect('/books/new')
 
-    sql = "INSERT INTO items (title, writer, year, isbn, publisher) VALUES (:title, :writer, :year, :isbn, :publisher)"
-    
-    #db.session.execute(sql, {
-    #    'title': title,
-    #    'writer': author,
-    #    'year': year,
-    #    'isbn': isbn,
-    #    'publisher': publisher
-    #})
-    #db.session.commit()
+    sql = text("INSERT INTO items (title, writer, year, isbn, publisher) VALUES (:title, :writer, :year, :isbn, :publisher)")
+    db.session.execute(sql, {
+       'title': title,
+       'writer': author,
+       'year': year,
+       'isbn': isbn,
+       'publisher': publisher
+    })
+    db.session.commit()
 
     flash('Kirja lisätty tietokantaan.', 'success')
     return redirect('/')
