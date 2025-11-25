1c104ae342c077cdbd68914655d1810b7368f855
Pinja Paivakumpu <paivakumpupinja@gmail.com>
2025-11-25 11:22:40 +0200

    pylint fixes

--- src/config.py
+++ src/config.py
@@ -1,9 +1,7 @@
-
+from os import getenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-from os import getenv
-from sqlalchemy import text
 
 load_dotenv()
 
