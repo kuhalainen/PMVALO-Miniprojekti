59d189d716ace98a8897c162c364975a5e0c9994
Akseli <182494262+kuhalainen@users.noreply.github.com>
2025-12-11 11:49:15 +0200

    Restore ISBN validation to fix tests

--- src/util.py
+++ src/util.py
@@ -18,8 +18,8 @@ def validate_book(title, author, year, isbn, publisher):
     if len(str(year)) != 4 or not str(year).isdigit():
         raise UserInputError("Error: Year must be a four-digit number.")
 
-    #if not (str(isbn).isdigit() and (len(str(isbn)) == 10 or len(str(isbn)) == 13)):
-    #    raise UserInputError("Error: ISBN must be a 10 or 13 digit number.")
+    if not (str(isbn).isdigit() and (len(str(isbn)) == 10 or len(str(isbn)) == 13)):
+        raise UserInputError("Error: ISBN must be a 10 or 13 digit number.")
 
     if int(year) > datetime.datetime.now().year:
         raise UserInputError("Error: Year cannot be set to the future.")
