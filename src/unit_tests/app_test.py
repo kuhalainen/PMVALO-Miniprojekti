2b187ef4e89e59a98b14f6ab5d3d78076f5a225a
Pinja Paivakumpu <paivakumpupinja@gmail.com>
2025-12-12 01:27:59 +0200

    fix app.py tests

--- src/unit_tests/app_test.py
+++ src/unit_tests/app_test.py
@@ -4,9 +4,6 @@ from app import app
 class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = app
-        self.app.config['TESTING'] = True
-        self.ctx = self.app.app_context()
-        self.ctx.push()
         self.client = self.app.test_client()
 
     def test_new_book_returns_200(self):
@@ -30,14 +27,6 @@ class TestApp(unittest.TestCase):
         response = self.client.get('/inproceedings/new')
         self.assertEqual(response.status_code, 200)
 
-    def test_inspect_bibtex_returns_200(self):
-        response = self.client.get('/inspect_bibtex')
-        self.assertEqual(response.status_code, 200)
-
     def test_index_sort_query_returns_200(self):
         resp = self.client.get('/?sort=author')
         self.assertEqual(resp.status_code, 200)
-
-    def test_download_bibtex_returns_200(self):
-        response = self.client.get('/download_bibtex')
-        self.assertEqual(response.status_code, 200)
